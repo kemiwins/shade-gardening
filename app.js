ee9cea39033efc0fbb17af6935eb299dd824f213
kemiwins <kemi.mail@gmail.com>
2014-11-19 22:47:18 -0500

    fixed footer in ie and chrome, added dogwood photo

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 $(document).ready(function(){
 
-	$(".about").hide();
+	$("#about").hide();
 	
 	$("#annuals-button").click(function(){
 		$(".annual").show();
 		$(".perennial").hide();
 		$(".shrub").hide();
-		$(".about").hide();	
+		$("#about").hide();	
 	})
 	$("#perennials-button").click(function(){
 		$(".annual").hide();
 		$(".perennial").show();
 		$(".shrub").hide();
-		$(".about").hide();		
+		$("#about").hide();		
 	})
 	$("#shrubs-button").click(function(){
 		$(".annual").hide();
 		$(".perennial").hide();
 		$(".shrub").show();
-		$(".about").hide();		
+		$("#about").hide();		
 	})
 	$("#all-button").click(function(){
 		$(".annual").show();
 		$(".perennial").show();
 		$(".shrub").show();
-		$(".about").hide();	
+		$("#about").hide();	
 	})
 		$("#about-button").click(function(){
 		$(".annual").hide();
 		$(".perennial").hide();
 		$(".shrub").hide();
-		$(".about").show();	
+		$("#title img").show();
+		$("#about").show();	
 	})
 
 
