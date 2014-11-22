07c980c8408505e407bca1e8963eac0db59182d0
kemiwins <kemi.mail@gmail.com>
2014-11-22 15:48:52 -0500

    changed images to jpg, added about page + links

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
 $(document).ready(function(){
-
-	$("#about").hide();
 	
 	$("#annuals-button").click(function(){
 		$(".annual").show();
 		$(".perennial").hide();
 		$(".shrub").hide();
-		$("#about").hide();	
 	})
 	$("#perennials-button").click(function(){
 		$(".annual").hide();
 		$(".perennial").show();
-		$(".shrub").hide();
-		$("#about").hide();		
+		$(".shrub").hide();	
 	})
 	$("#shrubs-button").click(function(){
 		$(".annual").hide();
 		$(".perennial").hide();
-		$(".shrub").show();
-		$("#about").hide();		
+		$(".shrub").show();	
 	})
 	$("#all-button").click(function(){
 		$(".annual").show();
 		$(".perennial").show();
 		$(".shrub").show();
-		$("#about").hide();	
 	})
-		$("#about-button").click(function(){
-		$(".annual").hide();
-		$(".perennial").hide();
-		$(".shrub").hide();
-		$("#about").show();	
-	})
-
-
 });
